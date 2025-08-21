ccca76e2e55eb8ca0dd1d0d9a9d51b6d400cca78
Aidar Nigmatullin <gudwinthegreat@gmail.com>
2025-08-21 14:29:50 +0300

    fix api url

--- apps/front/src/environments/environment.prod.ts
+++ apps/front/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000'
+  apiUrl: 'https://84.39.245.249/:3000'
 };
--- apps/front/src/environments/environment.ts
+++ apps/front/src/environments/environment.ts
@@ -5,7 +5,7 @@ declare function require(s: string): any;
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000'
+  apiUrl: 'https://84.39.245.249/:3000'
 };
 
 
