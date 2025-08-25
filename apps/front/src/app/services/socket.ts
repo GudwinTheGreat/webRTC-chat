8b21f8c5f380de872439f30f25df252588903b3f
Aidar Nigmatullin <gudwinthegreat@gmail.com>
2025-08-25 11:43:56 +0300

    chore

--- apps/front/src/app/services/socket.ts
+++ apps/front/src/app/services/socket.ts
@@ -1,5 +1,4 @@
 import { io, Socket } from "socket.io-client";
-import { environment } from "../../environments/environment";
 import { Injectable } from "@angular/core";
 
 @Injectable({ providedIn: 'root' })
